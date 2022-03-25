3d928103e5e9f21a60a76eeafdd76b30bbae0e35
ExplosivePro <explosive0105@gmail.com>
2022-03-25 20:53:17 +0000

    Project structure initialized

---
+++ src/components/WHandler/index.tsx
@@ -0,0 +1,7 @@
+import {WHandlerProps} from './type'
+
+export default function WHandler(props: WHandlerProps) {
+    return (
+        <div> This is Handler </div>
+    )
+}
---
+++ src/components/WNode/index.tsx
@@ -0,0 +1,7 @@
+import {WNodeProps} from './type'
+
+export default function WNode(props: WNodeProps) {
+    return (
+        <div> This is WNode </div>
+    )
+}
